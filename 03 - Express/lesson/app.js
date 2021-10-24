deb4528653771b586166a920981e77b3dfbd3c3c
Hari om Singh <www.hariom03.com@gmail.com>
2021-10-24 15:54:16 +0530

    add rest api note

--- 03 - Express/lesson/app.js
+++ 03 - Express/lesson/app.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/json", (req, res) => {
+  res
+    // .status(200)
+    .json({ msg: "hello world from server in json", app: "express" });
+});
 // server the app
 const port = 3000;
 app.listen(port, () => {
