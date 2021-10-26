61f2524d6abc44d22ea8df1afe3960ad96b59675
Hari om Singh <www.hariom03.com@gmail.com>
2021-10-26 13:40:44 +0530

    add a html using express

--- 03 - Express/lesson/app.js
+++ 03 - Express/lesson/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 // set express
 const app = express();
 
+const path = require("path");
 // create routes
 
 app.get("/", (req, res) => {
@@ -13,6 +14,10 @@ app.get("/json", (req, res) => {
   res
     // .status(200)
     .json({ msg: "hello world from server in json", app: "express" });
+});
+// server a html file using express
+app.get("/user", (req, res) => {
+  res.sendFile(path.join(__dirname, "./user.html"));
 });
 // server the app
 const port = 3000;
