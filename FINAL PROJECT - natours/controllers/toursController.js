c1e364655518005e84ae9d9ba4c710afe7a61447
Hari om Singh <www.hariom03.com@gmail.com>
2021-11-20 21:26:17 +0530

    add delete method in toursController using mongoose

--- FINAL PROJECT - natours/controllers/toursController.js
+++ FINAL PROJECT - natours/controllers/toursController.js
@@ -85,4 +85,17 @@ exports.updateTour = async (req, res) => {
     console.log(error);
   }
 };
-exports.deleteTour = (req, res) => {};
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      status: "fail",
+    });
+  }
+};
