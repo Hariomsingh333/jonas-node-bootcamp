cf6a93ca35c3a22e8522be8c0e68a05e7177130d
Hari om Singh <www.hariom03.com@gmail.com>
2021-11-20 21:13:42 +0530

    update using mongoose

--- FINAL PROJECT - natours/controllers/toursController.js
+++ FINAL PROJECT - natours/controllers/toursController.js
@@ -43,22 +43,6 @@ exports.getTour = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-  // console.log(req.params); // {id: "5"}
-  // convert id string to number just multiply
-  // const id = req.params.id * 1;
-  // const tour = tours.find((el) => el.id === id);
-  // if (!tour) {
-  //   return res.status(404).json({
-  //     status: "fall",
-  //     message: "wrong id 404 page",
-  //   });
-  // }
-  // res.status(200).json({
-  //   status: "success",
-  //   data: {
-  //     tour,
-  //   },
-  // });
 };
 
 exports.createTour = async (req, res) => {
@@ -79,25 +63,26 @@ exports.createTour = async (req, res) => {
     });
   }
 };
-exports.updateTour = (req, res) => {
-  // res.status(200).json({
-  //   status: "success",
-  //   data: {
-  //     tours: "<Update .........>",
-  //   },
-  // });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.prams.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      data: {
+        error,
+      },
+    });
+    console.log(error);
+  }
 };
-exports.deleteTour = (req, res) => {
-  // 204 means not content, when we handle with delete method we use 204 status code.
-  // if (req.params.id * 1 > tours.length) {
-  //   console.log(`here is your param ${val}`);
-  //   return res.status(404).json({
-  //     status: "fail",
-  //     message: "you id is wrong 404 not found",
-  //   });
-  // }
-  // res.status(204).json({
-  //   status: "success delete",
-  //   data: "null",
-  // });
-};
+exports.deleteTour = (req, res) => {};
